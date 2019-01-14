8d809f77b01dd56d6040606b8e3d70eb123ab832
Rodolfo Vieira Valentim <rodolfovieiravalentim@gmail.com>
2019-01-14 21:38:59 -0200

    Better looks

--- src/components/game/game.ts
+++ src/components/game/game.ts
@@ -30,11 +30,12 @@ export class GameComponent {
   private hill2Speed: number = 50;
 
   private images: Map<string, any>;
+  private start: number;
 
   private message = {
-		author: 'tutorialedge',
-		message: 'this is a test message'
-	}
+    author: 'tutorialedge',
+    message: 'this is a test message'
+  }
 
   constructor(private messager: MessagerProvider) {
     messager.create('http://localhost:5000/')
@@ -45,50 +46,62 @@ export class GameComponent {
   }
 
   sendMsg() {
-		console.log('Send message from client to websocket: ', this.message);
-		this.messager.sendMsg(this.message);
-	}
+    console.log('Send message from client to websocket: ', this.message);
+    this.messager.sendMsg(this.message);
+  }
 
   ngAfterViewInit() {
     // drawing the game canvas from scratch here
     // In future we can pass stages as param and load indexes from arrays of background elements etc
+    console.log(document.getElementById("column-player-1").clientWidth)
 
     if (this.stage) {
-    this.stage.autoClear = true;
-    this.stage.removeAllChildren();
-    this.stage.update();
+      this.stage.autoClear = true;
+      this.stage.removeAllChildren();
+      this.stage.update();
     } else {
-        this.stage = new createjs.Stage(this.name);
+      this.stage = new createjs.Stage(this.name);
     }
+
+    this.stage.canvas.width = document.getElementById("column-player-1").clientWidth - 10
     this.w = this.stage.canvas.width;
     this.h = this.stage.canvas.height;
     this.manifest = [
-        {src: "assets/imgs/spritesheet_grant.png", id: "grant"},
-        {src: "assets/imgs/sky.png", id: "sky"},
-        {src: "assets/imgs/ground.png", id: "ground"},
-        {src: "assets/imgs/hill1.png", id: "hill"},
-        {src: "assets/imgs/hill2.png", id: "hill2"}
+      { src: "assets/imgs/spritesheet_grant.png", id: "grant" },
+      { src: "assets/imgs/sky.png", id: "sky" },
+      { src: "assets/imgs/ground.png", id: "ground" },
+      { src: "assets/imgs/hill1.png", id: "hill" },
+      { src: "assets/imgs/hill2.png", id: "hill2" }
     ];
 
     this.loadImages(this.manifest, this.drawGame)
   }
-  
+
   tick(event) {
     this.stage.update(event);
-		var deltaS = event.delta / 1000;
-		var position = this.grant.x + this.grantSpeed * deltaS;
-		var grantW = this.grant.getBounds().width * this.grant.scaleX;
-		this.grant.x = (position >= this.w + grantW) ? - grantW : position;
-		this.ground.x = (this.ground.x - deltaS * this.grantSpeed) % this.ground.tileW;
-		this.hill.x = (this.hill.x - deltaS * this.hillSpeed);
-		if (this.hill.x + this.hill.image.width * this.hill.scaleX <= 0) {
-			this.hill.x = this.w;
-		}
-		this.hill2.x = (this.hill2.x - deltaS * this.hill2Speed);
-		if (this.hill2.x + this.hill2.image.width * this.hill2.scaleX <= 0) {
-			this.hill2.x = this.w;
-		}
-		this.stage.update(event);
+    var deltaS = event.delta / 1000;
+    var position = this.grant.x + this.grantSpeed * deltaS;
+    var grantW = this.grant.getBounds().width * this.grant.scaleX;
+
+    if (position >= this.w) {
+      this.grant.x = - grantW
+      console.log(this.start)
+      this.start = 0
+    }
+    else {
+      this.grant.x = position
+      this.start += deltaS
+    }
+    this.ground.x = (this.ground.x - deltaS * this.grantSpeed) % this.ground.tileW;
+    this.hill.x = (this.hill.x - deltaS * this.hillSpeed);
+    if (this.hill.x + this.hill.image.width * this.hill.scaleX <= 0) {
+      this.hill.x = this.w;
+    }
+    this.hill2.x = (this.hill2.x - deltaS * this.hill2Speed);
+    if (this.hill2.x + this.hill2.image.width * this.hill2.scaleX <= 0) {
+      this.hill2.x = this.w;
+    }
+    this.stage.update(event);
   }
 
   drawGame(scope) {
@@ -111,7 +124,7 @@ export class GameComponent {
     let spriteSheet = new createjs.SpriteSheet({
       framerate: 30,
       "images": [scope.images.get('grant')],
-      "frames": {"regX": 82, "height": 292, "count": 64, "regY": 0, "width": 165},
+      "frames": { "regX": 82, "height": 292, "count": 64, "regY": 0, "width": 165 },
       "animations": {
         "run2": [0, 25, "run2", 2.0],
         "run1": [0, 25, "run1", 1.0],
@@ -120,32 +133,32 @@ export class GameComponent {
     });
 
     scope.grant = new createjs.Sprite(spriteSheet, "run1");
-		scope.grant.y = 35;
+    scope.grant.y = 35;
     scope.stage.addChild(scope.sky, scope.hill, scope.hill2, scope.ground, scope.grant);
 
     scope.stage.addEventListener("stagemousedown", scope.handleJumpStart.bind(scope));
-		createjs.Ticker.timingMode = createjs.Ticker.RAF;
-		createjs.Ticker.addEventListener("tick", scope.tick.bind(scope));
+    createjs.Ticker.timingMode = createjs.Ticker.RAF;
+    createjs.Ticker.addEventListener("tick", scope.tick.bind(scope));
   }
-  
+
   loadImages(manifest: any, callback: Function): void {
     this.images = new Map<string, any>()
     let loadedImages = 0;
 
-    for(let im in manifest){
+    for (let im in manifest) {
       let image = new Image();
       image.src = manifest[im].src;
-      image.onload = () => { 
+      image.onload = () => {
         loadedImages++;
-        if (loadedImages == manifest.length){
+        if (loadedImages == manifest.length) {
           callback(this)
         }
-      } 
+      }
       this.images.set(manifest[im].id, image);
     }
   }
 
   handleJumpStart(scope) {
-		this.grant.gotoAndPlay("jump");
-	}
+    this.grant.gotoAndPlay("jump");
+  }
 }
